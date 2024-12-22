46f1d3caffc30207c33a56cda940e2807d95e4c9
爱玩电脑的WindowsXP <132193112+AWDNDWindowsXP@users.noreply.github.com>
2024-12-22 18:30:08 +0800

    更改2个功能
    1.更改插件加载功能
    2.微调Plug页面

--- Win_Design/Pages/Main_Page_SubProject/Home.xaml.cs
+++ Win_Design/Pages/Main_Page_SubProject/Home.xaml.cs
@@ -36,7 +36,17 @@ namespace Win_Design.Pages.Main_Page_SubProject
                 }
                 else
                 {
-                    iNKORE.UI.WPF.Modern.Controls.MessageBox.Show($"未找到文件：{fileName}", "插件加载错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    if (pluginName == "default")
+                    {
+                        var fs = File.Create("default.json");
+                        fs.Close();
+                        File.WriteAllText("default.json", "{\r\n  \"Main\": [\r\n    {\r\n      \"Name\": \"Wellcome\",\r\n      \"Text\": \"欢迎使用 Win-Design 设计器\",\r\n      \"Type\": \"Label\",\r\n      \"FontSize\": 18,\r\n      \"x\": 230,\r\n      \"y\": 10,\r\n      \"Wid\": 500,\r\n      \"Hei\": 80\r\n    },\r\n    {\r\n      \"Name\": \"Help\",\r\n      \"Text\": \"创建项目和打开项目请前往\\\"项目\\\"选项卡\",\r\n      \"Type\": \"Label\",\r\n      \"FontSize\": 15,\r\n      \"x\": 10,\r\n      \"y\": 60,\r\n      \"Wid\": 500,\r\n      \"Hei\": 80\r\n    },\r\n    {\r\n      \"Name\": \"Bilibili\",\r\n      \"Text\": \"Bilibili\",\r\n      \"Type\": \"Button\",\r\n      \"FontSize\": 15,\r\n      \"Command\": \"start https://space.bilibili.com/1527364468\",\r\n      \"x\": 10,\r\n      \"y\": 90,\r\n      \"Wid\": 100,\r\n      \"Hei\": 30\r\n    }\r\n  ]\r\n\r\n}");
+                        Cs.Plug.Load_Home.Load_Home_For_Json(File.ReadAllText(fileName));
+                    }
+                    else
+                    {
+                        iNKORE.UI.WPF.Modern.Controls.MessageBox.Show($"未找到文件：{fileName}", "插件加载错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
                 }
             }
             catch(Exception ex){
